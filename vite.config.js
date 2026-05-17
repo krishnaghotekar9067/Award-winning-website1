fd41e7389fc95eb47f64fe98de3c9e017d9cf0cf
krishna ghotekar <krishnaghotekar1@gmail.com>
2026-05-17 06:42:56 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Award-winning-website1/',
+  base: './',
   plugins: [react(),tailwindcss()],
-  
+
 })
