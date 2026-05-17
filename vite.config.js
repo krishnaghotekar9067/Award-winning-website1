86ee8b13aaf5fdeef2bd1477e9603756a75f7d72
Krishna Ghotekar <krishnaghotekar1@gmail.com>
2026-05-17 05:39:56 +0100

    fix homepage paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Award-winning-website1/',
   plugins: [react(),tailwindcss()],
 })
